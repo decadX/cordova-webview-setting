b4245b23a1cfc0c6632c7899203301421b648637
decadX <decadX@users.noreply.github.com>
2021-12-30 10:14:44 +0000

    Update WebviewSetting.java

--- src/android/WebviewSetting.java
+++ src/android/WebviewSetting.java
@@ -1,34 +1,57 @@
 package com.darktalker.cordova.webviewsetting;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CallbackContext;
+import org.apache.cordova.*;
+import org.apache.cordova.engine.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.os.Build;
+import android.webkit.*;
 
 import android.util.Log;
 
+
 public class WebviewSetting extends CordovaPlugin {
-    private CordovaWebView webView;
+    private CordovaWebView wv;
     private static final String LOG_TAG = "WebviewSetting";
     @Override
     public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
-        Log.d(LOG_TAG, "Webview flags initialized");
-        this.webView = webView;
+        this.wv = webView;
         super.initialize(cordova, webView); 
+		setWebviewFlags();
         
     }
+	
+    private void setWebviewFlags() {
+		// needed for sdk 30 - sqlite plugin db access
+		cordova.getActivity().runOnUiThread(new Runnable() {
+			public void run() {
+				CordovaWebViewImpl webViewImpl = (CordovaWebViewImpl) wv;
+				SystemWebViewEngine engine = (SystemWebViewEngine) webViewImpl.getEngine();
+				WebView webView = (WebView) engine.getView();
+				WebSettings settings = webView.getSettings();	
+				// needed for sdk 30 - sqlite plugin db access
+				settings.setAllowFileAccess(true);
+				Log.d(LOG_TAG, "Webview flags setAllowFileAccess set to true");
+				settings.setAllowContentAccess(true);
+				Log.d(LOG_TAG, "Webview flags setAllowContentAccess set to true");
+				settings.setAllowFileAccessFromFileURLs(true);
+				Log.d(LOG_TAG, "Webview flags setAllowFileAccessFromFileURLs set to true");
+				
+				callbackContext.success();
+			}
+		});
+    }
+	
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if ("set".equals(action)) {
             cordova.getActivity().runOnUiThread(new Runnable() {
-                public void run() {
-
-					final WebSettings settings = webView.getSettings();
-					
+				public void run() {
+					CordovaWebViewImpl webViewImpl = (CordovaWebViewImpl) wv;
+					SystemWebViewEngine engine = (SystemWebViewEngine) webViewImpl.getEngine();
+					WebView webView = (WebView) engine.getView();
+					WebSettings settings = webView.getSettings();	
 					// needed for sdk 30 - sqlite plugin db access
 					settings.setAllowFileAccess(true);
 					Log.d(LOG_TAG, "Webview flags setAllowFileAccess set to true");
@@ -36,9 +59,9 @@ public class WebviewSetting extends CordovaPlugin {
 					Log.d(LOG_TAG, "Webview flags setAllowContentAccess set to true");
 					settings.setAllowFileAccessFromFileURLs(true);
 					Log.d(LOG_TAG, "Webview flags setAllowFileAccessFromFileURLs set to true");
-			
-                    callbackContext.success();
-                }
+					
+					callbackContext.success();
+				}
             });
             return true;
         }
